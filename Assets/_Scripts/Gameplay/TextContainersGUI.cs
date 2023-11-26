8afb76728bb56e4090eeeb6ed914d6f4959b6a89
Florian Lernould <florianlernould7@gmail.com>
2023-11-26 12:45:08 +0100

    feat: Add navigation through text content
    Player can navigates through the differents sentences when reading an object content.

--- Assets/_Scripts/Gameplay/TextContainersGUI.cs
+++ Assets/_Scripts/Gameplay/TextContainersGUI.cs
@@ -1,10 +1,12 @@
 ﻿using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TextContainersGUI : MonoBehaviour
 {
     [SerializeField] private GameObject content;
     [SerializeField] private TextMeshProUGUI textMesh;
+    [SerializeField] private GameObject leftArrow, rightArrow;
 
     private string[] _textContent;
     private int _currentIndex = 0;
@@ -22,27 +24,67 @@ public class TextContainersGUI : MonoBehaviour
         _textContent = content;
         _currentIndex = 0;
 
-        string text = GetSentenceFromContent();
-        SetText(text);
+        UpdateTextContent();
         EnableContentPanel();
-    }
-
-    /// <summary>
-    /// Get the string in the sentence array at current index
-    /// </summary>
-    private string GetSentenceFromContent()
-    {
-        int index = Mathf.Clamp(_currentIndex, 0, _textContent.Length - 1);
-        return _textContent[index];
     }
 
     /// <summary>
     /// Set the text in the TextMesh component
     /// </summary>
     /// <param name="text"> Text content </param>
-    private void SetText(string text)
+    private void UpdateTextContent()
     {
-        textMesh.text = text;
+        textMesh.text = GetSentence();
+        EnableNavigationArrows();
+    }
+
+    /// <summary>
+    /// Get the string in the sentence array at current index
+    /// </summary>
+    private string GetSentence()
+    {
+        int index = Mathf.Clamp(_currentIndex, 0, _textContent.Length - 1);
+        return _textContent[index];
+    }
+
+    /// <summary>
+    /// Enable navigation arrows when the text content is shown
+    /// </summary>
+    private void EnableNavigationArrows()
+    {
+        if (!rightArrow || !leftArrow)
+            return;
+
+        int maxIndex = _textContent.Length;
+        bool canNext = _currentIndex < maxIndex - 1;
+        bool canPrevious = _currentIndex > 0 && _currentIndex < maxIndex;
+
+        rightArrow.SetActive(canNext);
+        leftArrow.SetActive(canPrevious);
+    }
+
+    /// <summary>
+    /// Get the previous sentence in text content and update UI
+    /// </summary>
+    private void GetPreviousSentence()
+    {
+        if (_currentIndex <= 0)
+            return;
+
+        _currentIndex--;
+        UpdateTextContent();
+    }
+
+    /// <summary>
+    /// Get the next sentence in text content and update UI
+    /// </summary>
+    private void GetNextSentence()
+    {
+        if (_currentIndex >= _textContent.Length - 1)
+            return;
+
+        _currentIndex++;
+        UpdateTextContent();
     }
 
     /// <summary>
