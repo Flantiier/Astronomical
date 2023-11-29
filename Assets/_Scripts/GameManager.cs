3954ce5a2d6f03ffb2bf9c9ba47916a671d15d6d
Florian Lernould <florianlernould7@gmail.com>
2023-11-29 17:34:16 +0100

    Create GameManager and add colliders to shelves
    Create a GameManager script with a function to enable/disable Cursor.
    Add colliders on shelves prefabs.

---
+++ Assets/_Scripts/GameManager.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+            Destroy(gameObject);
+        else
+            Instance = this;
+
+        EnableCursor(false);
+    }
+
+    public void EnableCursor(bool enabled)
+    {
+        Cursor.visible = enabled;
+    }
+}
