d84f7f783b08d64ad1257014990d8f307717b139
Florian Lernould <florianlernould7@gmail.com>
2023-11-29 17:53:34 +0100

    Add black flashlight script
    Created a new script to enable/disable flashlight.
    Modified light and emissive texture colors.

---
+++ Assets/_Scripts/Gameplay/BlackFlashlight.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class BlackFlashlight : PickableObject
+{
+    [SerializeField] private RevealingShaderSource revealingScript;
+
+    private void Start()
+    {
+        EnableRevealingScript(false);
+    }
+
+    public override void SetObjectParent(Transform parent)
+    {
+        base.SetObjectParent(parent);
+        EnableRevealingScript(true);
+    }
+
+    public override void ResetObjectParent(Vector3 position)
+    {
+        base.ResetObjectParent(position);
+        EnableRevealingScript(false);
+    }
+
+    /// <summary>
+    /// Enable revealing script on this object
+    /// </summary>
+    private void EnableRevealingScript(bool enabled)
+    {
+        if (!revealingScript)
+            return;
+
+        revealingScript.enabled = enabled;
+    }
+}
